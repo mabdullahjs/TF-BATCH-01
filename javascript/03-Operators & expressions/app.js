32b6d475b4f7a7067a9475e02a5a02d86672cee3
Muhammad Abdullah <101110601+mabdullahjs@users.noreply.github.com>
2023-10-22 18:25:59 +0500

    conditional statement

---
+++ javascript/03-Operators & expressions/app.js
@@ -0,0 +1,538 @@
+// console.log('hello world');
+
+
+
+
+// //Arithmatic Operator
+
+// // +
+// // -
+// // *
+// // /
+// // %
+
+// //Assignment Operator
+// // =
+// // +=
+// // -=
+// // /=
+// // *=
+
+// //Comparision Operator
+
+// // > greater than
+// // < less than
+// // == equal to
+// // === double equal to
+// // != not equal to
+// // >= greater than and equal to
+// // <= less than and equal to
+
+// //logical Operator
+
+// //&&
+// //||
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const num = 10;
+// const num2 = 20;
+// const solution = num + num2;
+// // console.log(solution);
+// // console.log(num + num2);
+// console.log('addition ===>',10 + 20);
+// console.log('subtraction ===>',10 - 20);
+// console.log('multiply ===>',10 * 20);
+// console.log('division ===>',10 / 20);
+// console.log('remainder ===>',4 % 3);
+// console.log('squaring ===>',10 ** 2);
+
+
+
+// var name = 'abdullah';
+// var name = 'usman'
+// console.log(name);
+// console.log(name);
+
+
+
+
+
+
+
+
+
+
+// const num1 = 10;
+// const num2 = 20;
+
+// console.log("num1 modulus num2 is equal to " + num1 % num2);
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log('hello world');
+// let totalMarks = 100;
+// let obtained = 70;
+// let percentage = obtained / totalMarks * 100
+// console.log(percentage + "%");
+
+
+
+
+
+
+
+
+// let greetUser;
+// let greetUser;
+// console.log(greetUser);
+
+// let names = 'abdullah'
+// greetUser = 'hello' + names
+// const abc = 'abdullah';
+
+
+
+
+
+
+// let userId = '10';
+// let numUserId = +userId;
+// console.log(numUserId + 10);
+
+
+// let id;
+// id = Boolean(id);
+// console.log(id);
+
+
+
+
+
+
+
+// alert('hello world');
+// const userName = Number(prompt('enter your name'));
+// console.log(typeof(userName));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//percentage calculator
+
+// let studentName = prompt('enter your name');
+// let chemMarks = +prompt('Enter chem marks');
+// let phyMarks = +prompt('Enter physics marks');
+// let mathsMarks = +prompt('Enter maths marks');
+// let bioMarks = +prompt('Enter biology marks');
+
+
+
+// let totalMarks = 400;
+// let userTotal = chemMarks + phyMarks + mathsMarks + bioMarks;
+// let percentage = userTotal / totalMarks * 100;
+
+
+// console.log(studentName + " total marks is "+ userTotal + " and its percentage is "+ percentage);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//assignment operator
+
+
+
+// let num1 = 10;
+// // num1 = num1 + 2;
+// num1 +=  2;
+// num1 *= 2;
+// num1 /=2;
+// num1 -= 2;
+// num1 %= 3;
+// console.log(num1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//comparision operators
+
+
+// let num1 = 10;
+// let num2 = '10';
+// let comparision = num1 === num2;
+// console.log(typeof comparision , comparision);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//logical operators
+
+// && AND == URDU == OOR
+// || OR == URDU == YAA
+
+
+
+// let num1 = 10;
+// let num2 = '10';
+// let compareVal = num1 > num2 || num1 <= num2 || num1 == num2;
+// let compareVal = num1 > num2 && num1 <= num2 && num1 == num2;
+// console.log(typeof(compareVal) , compareVal);
+
+
+
+
+
+// let compareVal = num1 > num2 || num1 == num2 && num1 < num2 || num2 > num2 && num1 >= num2 || num2 === num1;
+// let compareVal = num1 < num2 || num1 == num2 && num1 < num2 || num2 < num2 && num1 < num2 || num2 !== num1;
+
+// let num1 = 10;
+// let num2 = '10';
+
+// let compareVal =  true;
+// console.log(typeof(compareVal) , compareVal);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+
+
+
+
+// if (10 < 15){
+//     console.log('if chal rha ha');
+// }else{
+//     console.log('else chal rha ha');
+// }
+
+
+
+
+
+// const bankBalance = 2000000;
+// const civic = false
+// if(bankBalance > 800000 && civic === true){
+//     console.log('rishta pakka ha. dadi bhi saath ayengi');
+//     console.log('Mithai leta hua ana');
+// }else if(bankBalance >= 2000000 && civic === false){
+//     console.log('abba nahi maan rha..');
+// }
+// else{
+//     console.log('rooh tumhara pass haa ...');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(){
+
+// }else if(){
+
+// }else if(){
+
+// }else{
+
+// }
+
+
+// console.log('hello world')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const bankBalance = 2000000;
+const car = 'fortuner';
+const apnaHome = true;
+
+if(bankBalance > 500000 && car === 'fortuner' && apnaHome === true){
+    console.log('rishta mubarak hoo');
+    console.log('Mithai leta hua ana');
+    console.log('dadi bhi saath ayengi');
+} else if(bankBalance > 500000 || car ==='mehran' && apnaHome === false){
+    console.log('istekhara ma naa aya ha');
+} else if(bankBalance > 150000 && car === 'civic' && apnaHome === true){
+    console.log('abba maan gaye hain');
+}else{
+    console.log('istekhara ma naa aya ha , abba nahi maan rha , ammi nas kaat laingi..');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
