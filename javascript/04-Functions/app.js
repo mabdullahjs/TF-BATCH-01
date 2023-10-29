631ee473163d7bf758b3f5036d21f7c97b695a1f
Muhammad Abdullah <101110601+mabdullahjs@users.noreply.github.com>
2023-10-29 18:19:29 +0500

    functions completed

---
+++ javascript/04-Functions/app.js
@@ -0,0 +1,403 @@
+// console.log('hello world');
+
+// let 
+// var
+// const
+
+
+// var user = 'abdullah';
+// var user = 'hamza'; // redecleration
+// user = 'ahmed' // reaasign
+
+
+
+// let user = 'abdullah';
+// user = 'rameez'; //reassign
+// let user = 'ahmed' // redeclaration
+
+
+
+//let users;
+//var userss;
+// const user;
+
+
+
+
+
+
+
+
+// const comparision = 3 != "3";
+// console.log(comparision);
+
+// ==
+// ===
+// !=
+// !==
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (3 > 3 || 3 < 3 && 3 >= 5 || 3 <= 50){
+//     console.log('if chala ga');
+// }else{
+//     console.log('else chala ga');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+
+
+
+// function repeatConsole() {
+//     console.log('hello world');
+//     console.log('hello world');
+//     console.log('hello world');
+// }
+// repeatConsole()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let names = prompt('enter your name');
+// let age = +prompt('enter your age');
+
+
+// function greetUser(userName , age) {
+//     if (isNaN(+names) && typeof (age) === 'number') {
+//         console.log('hello ' + userName + ' and my age is ' + age);
+//     } else {
+//         console.log('bhaijaaan sahi value do');
+//     }
+// }
+
+
+// greetUser(names , age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sum(num1, num2) {
+//     console.log(num1 + num2);
+//     return num1 + num2
+// }
+
+// sum(20 , 30);
+// console.log(sum(20, 30));
+// console.log(sum(20 , 30));
+
+
+
+
+
+
+
+
+
+
+// function gharKaNoker(samaan1 , samaan2){
+//     // console.log(samaan1 , samaan2);
+//     let returnVal =  samaan1 +" "+ samaan2
+//     return returnVal
+// }
+// gharKaNoker('doodh' , 'dhaniya');
+// // console.log();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function ramooKaka (dish1 , dish2 , desert){
+//     return "yelo bhaijan appka khana " + dish1 + " or " + dish2 + " or meetha ma " + desert + " haa"
+// }
+
+
+// console.log(ramooKaka('tindaa' , 'roti' , 'gurrr'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
