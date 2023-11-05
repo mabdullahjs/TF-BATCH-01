3a3c20c951ba9359fb1e07f1fb122b0e37a27e41
Muhammad Abdullah <101110601+mabdullahjs@users.noreply.github.com>
2023-11-05 19:03:20 +0500

    array and dom manipulation

--- javascript/05-DOM-Manipulation/app.js
+++ javascript/05-DOM-Manipulation/app.js
@@ -1,5 +1,165 @@
 // console.log('hello world');
 
+//create one prompt and on button show on screen hello + prompt value
+
+// const head = document.querySelector('#head');
+
+// // head.innerHTML = 'changed through javascript'
+// // console.log(head.innerHTML);
+
+
+// function greetUser(){
+//     if(head.innerHTML == 'Hello abdullah'){
+//         head.innerHTML = 'Hello world'
+//     }else{
+//         head.innerHTML = 'Hello abdullah';
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const head = document.querySelector('#head-one');
+
+
+// const userName = prompt('enter your name');
+
+
+// function greetUser(){
+//     head.innerHTML = "hello " + userName;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// const email = document.querySelector('#email');
+// const password = document.querySelector('#password');
+
+
+// function showVal(){
+//     console.log(email.value);
+//     console.log(password.value);
+//     email.value = ''
+//     password.value = ''
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const valOne = document.querySelector('#val-1');
+// const valTwo = document.querySelector('#val-2');
+
+
+// function sumVal(){
+//     console.log(+valOne.value + +valTwo.value);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ javascript/06-Array/app.js
@@ -0,0 +1,171 @@
+// let fruit1 = 'apple';
+// let fruit2 = 'orange';
+// let fruit3 = 'banana';
+// let fruit4 = 'mango';
+// let fruit5 = 'grapes';
+
+
+
+
+// let fruit = ["apple" , 20 , true , ["watermelon" , "nashpati" , ["anaar" , "anaar2"]] , "orange" , "banana" , "mango" , "grapes"];
+// console.log(fruit[3][2][1]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//push
+//pop
+//unshift
+//shift
+//splice
+//slice
+//split
+//join
+//concat
+//sort
+//reverse
+//indexof
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(fruit[3]);
+
+
+
+// let fruit = ["apple" , "orange" , "banana" , "mango" , "grapes"];
+// fruit[0] = 'saib';
+// console.log(fruit);
+
+
+// fruit.push('watermelon');
+// fruit.pop();
+// fruit.unshift('watermelon');
+// fruit.unshift('watermelon');
+// fruit.shift();
+// let fruit = ["apple" , "orange" , "banana" , "mango" , "grapes"];
+// fruit.splice(3 , 1 , 'watermelon' , 'kharboza');
+// fruit.splice(0 , 1);
+// const newArr = fruit.slice(1);
+// console.log(newArr);
+// console.log(fruit);
+
+
+
+
+
+// console.log(fruit);
+
+
+
+
+
+
+
+
+
+
+//join
+//concat
+
+
+//sort
+//reverse
+//indexof
+
+
+
+
+
+// let names = 'Muhammad-Abdullah-khan';
+// let newArr = names.split('-');
+// console.log(newArr);
+
+
+
+
+// let arr1 = ['apple' , 'banana' , 'grapes'];
+// let arr2 = ['watermelon' , 'melon' , 'nashpati'];
+// const newArr = arr1.concat(arr2);
+// console.log(newArr);
+
+
+
+// const myName = ["Muhammad" , "Abdullah" , "khan"];
+// const names = myName.join('-');
+// console.log(names);
+
+
+
+
+const newArr = Array('names' , 'place');
+console.log(newArr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
